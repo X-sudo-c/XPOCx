c4b04f609af202df229057995a4735064bdfa139
reishi <x1dragonn@gmail.com>
2025-05-06 19:52:24 +0000

    learning

---
+++ typehinting/subs2.py
@@ -0,0 +1,10 @@
+import subprocess
+import click
+from typing import List
+
+
+
+
+#create a helper function 
+
+def run_command(cmd: List[str], input_data: str = None) -> str:
---
+++ typehinting/typingmodule.py
@@ -0,0 +1,13 @@
+# we are learning how to use the typing module 
+
+def myfunction(parameter:str) -> str:
+    """
+    This function takes a string parameter and returns it.
+    """
+    return f"prameter = {10 + parameter}"
+
+
+
+print(myfunction(100))
+
+
