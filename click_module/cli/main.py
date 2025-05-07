7949c5ca9462c72a09ede513fd1dfdd0be77e5b8
reishi <x1dragonn@gmail.com>
2025-05-07 19:26:51 +0000

    using click

--- click_module/cli/main.py
+++ click_module/cli/main.py
@@ -1,17 +1,28 @@
-import click
+import click 
+from typing import List
 
 
-VERSION = "0.1.0"
+
+version = "0.1.0"
 
 @click.command()
-@click.version_option(VERSION)
-@click.argument("--name", nargs=2)
-def profile(names):
-    print(names)
+@click.argument("name", nargs = 2)
+@click.option("--age", "-a", type = int , default = None)
+@click.option("-s","--shout", type=bool, is_flag =True)
+
+
+def profile(name:List[str], age:str, shout:bool ) -> None:
+    text = f"Hey there {" ".join(name)} you are {age} years old"
+
+    if shout:
+        print(text.upper())
+    else:
+        print(text)
 
 
 
 
 
-if __name__ =='__main__':
-    profile
+if __name__ == "__main__":
+    profile()
+
